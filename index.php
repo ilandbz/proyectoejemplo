b26cca44c8b7d3f6d85bb687eb4bba9ccbb50ea3
cristian <cristianfigueroa19@hotmail.com>
2022-03-30 13:01:51 -0500

    ultimos cambios

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>INDEX</title>
 </head>
 <body>
     <?php
@@ -12,5 +12,11 @@
     echo 'HOLA MUNDO';
     
     ?>
+    <ul>
+        <li>Home</li>
+        <li>Nosotros</li>
+        <li>Contactenos</li>
+        <li>Transparencia</li>
+    </ul>
 </body>
 </html>
