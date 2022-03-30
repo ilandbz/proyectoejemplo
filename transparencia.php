86d5d86c5783eb0de44d2e670102efb723dde367
cristian <cristianfigueroa19@hotmail.com>
2022-03-30 13:06:41 -0500

    creado transparencia.php

---
+++ transparencia.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Transparencia</title>
+</head>
+<body>
+    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eligendi eveniet non quos magni dolor omnis necessitatibus a sint debitis, cum sunt, magnam, itaque maiores nulla dolorem facere eaque! Aspernatur, soluta?
+</body>
+</html>
